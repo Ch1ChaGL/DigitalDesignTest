e7daf2ae2bb198e58de140377d1ba4312c6f8f52
Danil Markov <markov.danil.04@mail.ru>
2023-05-18 23:22:01 +0300

    Адаптив + меню бургер
    Signed-off-by: Danil Markov <markov.danil.04@mail.ru>

--- DigitalDesignTest/js/main.js
+++ DigitalDesignTest/js/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const burger = document.querySelector('.menu__burger');
+burger.addEventListener('click', event => {
+  burger.classList.toggle('active');
+  document.querySelector('.menu__body').classList.toggle('open');
+});
