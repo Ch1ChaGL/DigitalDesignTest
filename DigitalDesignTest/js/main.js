a7006509e0730e8cb37b42549d98e383c990a015
Danil Markov <markov.danil.04@mail.ru>
2023-05-18 23:26:09 +0300

    Signed-off-by: Danil Markov <markov.danil.04@mail.ru>

--- DigitalDesignTest/js/main.js
+++ DigitalDesignTest/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const burger = document.querySelector('.menu__burger');
+//Назначение класса active при нажатии на меню-бургер
 burger.addEventListener('click', event => {
   burger.classList.toggle('active');
   document.querySelector('.menu__body').classList.toggle('open');
