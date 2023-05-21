36eeaf439cb3c70d9da473ca8d309189246d5213
Danil Markov <markov.danil.04@mail.ru>
2023-05-21 11:22:40 +0300

    Signed-off-by: Danil Markov <markov.danil.04@mail.ru>

--- DigitalDesignTest/js/main.js
+++ DigitalDesignTest/js/main.js
@@ -1,56 +1,6 @@
 'use strict';
 
 // import { getDayInfo } from './date.js';
-
-const dates = [
-  '01.01.23',
-  '10.01.23',
-  '15.01.23',
-  '22.01.23',
-  '05.02.23',
-  '12.02.23',
-  '20.02.23',
-  '25.02.23',
-  '03.03.23',
-  '08.03.23',
-  '17.03.23',
-  '24.03.23',
-  '02.04.23',
-  '09.04.23',
-  '16.04.23',
-  '21.04.23',
-  '01.05.23',
-  '06.05.23',
-  '14.05.23',
-  '20.05.23',
-  '04.06.23',
-  '10.06.23',
-  '18.06.23',
-  '25.06.23',
-  '03.07.23',
-  '09.07.23',
-  '16.07.23',
-  '22.07.23',
-  '01.08.23',
-  '07.08.23',
-  '15.08.23',
-  '20.08.23',
-  '02.09.23',
-  '09.09.23',
-  '17.09.23',
-  '24.09.23',
-  '01.10.23',
-  '08.10.23',
-  '16.10.23',
-];
-
-
-
-
-
-
-'use strict';
-
 const daysOfWeek = [
   'Воскресенье',
   'Понедельник',
@@ -124,18 +74,6 @@ function getDayInfo(date) {
   return myDate;
 }
 
-// export {getDayInfo};
-
-
-//Думал так можно сделать, но потом понял, что так не работает :)
-/*
-function getWeekNumberInMonth2(date) {
-  const currentDate = new Date(date);
-  const currentDay = currentDate.getDate();
-  const weekNumber = Math.ceil(currentDay / 7);
-  return weekNumber;
-}
-*/
 
 
 
@@ -145,6 +83,52 @@ function getWeekNumberInMonth2(date) {
 
 
 
+
+
+
+
+
+const dates = [
+  '01.01.23',
+  '10.01.23',
+  '15.01.23',
+  '22.01.23',
+  '05.02.23',
+  '12.02.23',
+  '20.02.23',
+  '25.02.23',
+  '03.03.23',
+  '08.03.23',
+  '17.03.23',
+  '24.03.23',
+  '02.04.23',
+  '09.04.23',
+  '16.04.23',
+  '21.04.23',
+  '01.05.23',
+  '06.05.23',
+  '14.05.23',
+  '20.05.23',
+  '04.06.23',
+  '10.06.23',
+  '18.06.23',
+  '25.06.23',
+  '03.07.23',
+  '09.07.23',
+  '16.07.23',
+  '22.07.23',
+  '01.08.23',
+  '07.08.23',
+  '15.08.23',
+  '20.08.23',
+  '02.09.23',
+  '09.09.23',
+  '17.09.23',
+  '24.09.23',
+  '01.10.23',
+  '08.10.23',
+  '16.10.23',
+];
 
 const cards = document.querySelectorAll('.card');
 
@@ -159,14 +143,8 @@ const setDate = async (card, date) => {
   subtitleElement.insertAdjacentElement('afterend', dateElement);
 };
 
-// for (let i = 0; i < cards.length; i++) {
-//   setDate(cards[i], dates[i]);
-// }
+for (let i = 0; i < cards.length; i++) {
+  setDate(cards[i], dates[i]);
+}
 
-const loadDates = async () => {
-  for (let i = 0; i < cards.length; i++) {
-    await setDate(cards[i], dates[i]);
-  }
-};
 
-setTimeout(loadDates, 0);
