bcc10b8e1c07c467365a6c905aebd06598967061
Danil Markov <markov.danil.04@mail.ru>
2023-05-21 14:21:12 +0300

    Signed-off-by: Danil Markov <markov.danil.04@mail.ru>

--- DigitalDesignTest/js/date.js
+++ DigitalDesignTest/js/date.js
@@ -1,87 +1,100 @@
-// 'use strict';
+'use strict';
 
-// const daysOfWeek = [
-//   'Воскресенье',
-//   'Понедельник',
-//   'Вторник',
-//   'Среда',
-//   'Четверг',
-//   'Пятница',
-//   'Суббота',
-// ];
-// const monthOfYear = [
-//   'Января',
-//   'Февраля',
-//   'Марат',
-//   'Апреля',
-//   'Мая',
-//   'Июня',
-//   'Июля',
-//   'Августа',
-//   'Сентября',
-//   'Октября',
-//   'Ноября',
-//   'Декабря',
-// ];
+const daysOfWeek = [
+  'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+];
+const monthOfYear = [
+  'Января',
+  'Февраля',
+  'Марат',
+  'Апреля',
+  'Мая',
+  'Июня',
+  'Июля',
+  'Августа',
+  'Сентября',
+  'Октября',
+  'Ноября',
+  'Декабря',
+];
 
-// function getMonth(month) {
-//   return monthOfYear[month];
-// }
+function getMonth(month) {
+  return monthOfYear[month];
+}
 
-// function getWeekNumberInMonth(date) {
-//   const currentDate = new Date(date);
+function getWeekNumberInMonth(date) {
+  const currentDate = new Date(date);
+  const currentWeek = getWeekNumber(currentDate);
 
-//   const startOfMonth = new Date(
-//     currentDate.getFullYear(),
-//     currentDate.getMonth(),
-//     1,
-//   );
-//   const diff = currentDate.getTime() - startOfMonth.getTime();
+  const startOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1,
+  );
+  const startWeek = getWeekNumber(startOfMonth);
 
-//   const daysPassed = Math.floor(diff / (24 * 60 * 60 * 1000));
+  return currentWeek - startWeek + 1;
+}
 
-//   const weekNumber = Math.ceil((daysPassed + startOfMonth.getDay() + 1) / 7);
+function getWeekNumber(date) {
+  const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+  const daysPassed = Math.floor(
+    (date - firstDayOfYear) / (24 * 60 * 60 * 1000),
+  );
+  const weekNumber = Math.ceil(
+    (daysPassed + firstDayOfYear.getDay() + 1) / 7,
+  );
 
-//   return weekNumber;
-// }
+  return weekNumber;
+}
 
-// const getDayOfWeek = day => {
-//   if (day < 0 || day > 6) throw new Error('Не существует такого дня недели');
-//   return daysOfWeek[day];
-// };
+const getDayOfWeek = day => {
+  if (day < 0 || day > 6) throw new Error('Не существует такого дня недели');
+  if (!Number.isInteger(day)) return day + '';
+  return daysOfWeek[day];
+};
 
-// /**
-//  * Получение даты из формата “01.01.2022” в Суббота, 1 неделя Января 2022 года
-//  * @param {string} date “01.01.2022”
-//  * @return {string} newDate Суббота, 1 неделя Января 2022 года
-//  */
-// function getDayInfo(date) {
-//   const parts = date.split('.');
-//   const day = parts[0];
-//   const month = parts[1];
-//   const year = parts[2];
+/**
+ * Получение даты из формата “01.01.2022” в Суббота, 1 неделя Января 2022 года
+ * @param {string} date “01.01.2022”
+ * @return {string} newDate Суббота, 1 неделя Января 2022 года
+ */
+function getDayInfo(date) {
+  const parts = date.split('.');
+  const day = parseInt(parts[0]);
+  const month = parseInt(parts[1]) - 1;
+  const year = parts[2];
 
-//   const convertDay = `${month}.${day}.${year}`;
+  const convertDay = `${month}.${day}.${year}`;
 
-//   const newDate = new Date(convertDay);
+  const newDate = new Date();
+  newDate.setDate(day);
+  newDate.setMonth(month);
+  newDate.setYear(year);
 
-//   const dayOfWeek = getDayOfWeek(newDate.getDay());
-//   const weekNumber = getWeekNumberInMonth(newDate);
-//   const monthName = getMonth(newDate.getMonth());
+  const dayOfWeek = getDayOfWeek(newDate.getDay());
+  const weekNumber = getWeekNumberInMonth(newDate);
+  const monthName = getMonth(newDate.getMonth());
 
-//   const myDate = `${dayOfWeek}, ${weekNumber} неделя ${monthName} ${newDate.getFullYear()} года`;
-//   return myDate;
-// }
+  const myDate = `${dayOfWeek}, ${weekNumber} неделя ${monthName} ${newDate.getFullYear()} года`;
+  return myDate;
+}
 
-// // export {getDayInfo};
+export {getDayInfo};
 
 
-// //Думал так можно сделать, но потом понял, что так не работает :)
-// /*
-// function getWeekNumberInMonth2(date) {
-//   const currentDate = new Date(date);
-//   const currentDay = currentDate.getDate();
-//   const weekNumber = Math.ceil(currentDay / 7);
-//   return weekNumber;
-// }
-// */
+//Думал так можно сделать, но потом понял, что так не работает :)
+/*
+function getWeekNumberInMonth2(date) {
+  const currentDate = new Date(date);
+  const currentDay = currentDate.getDate();
+  const weekNumber = Math.ceil(currentDay / 7);
+  return weekNumber;
+}
+*/
--- DigitalDesignTest/js/main.js
+++ DigitalDesignTest/js/main.js
@@ -1,92 +1,12 @@
 'use strict';
 
-// import { getDayInfo } from './date.js';
-
-window.addEventListener('DOMContentLoaded', () => {
-  alert('Я тут');
-  const daysOfWeek = [
-    'Воскресенье',
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота',
-  ];
-  const monthOfYear = [
-    'Января',
-    'Февраля',
-    'Марат',
-    'Апреля',
-    'Мая',
-    'Июня',
-    'Июля',
-    'Августа',
-    'Сентября',
-    'Октября',
-    'Ноября',
-    'Декабря',
-  ];
-
-  function getMonth(month) {
-    return monthOfYear[month];
-  }
-
-  function getWeekNumberInMonth(date) {
-    const currentDate = new Date(date);
-
-    const startOfMonth = new Date(
-      currentDate.getFullYear(),
-      currentDate.getMonth(),
-      1,
-    );
-    const diff = currentDate.getTime() - startOfMonth.getTime();
-
-    const daysPassed = Math.floor(diff / (24 * 60 * 60 * 1000));
-
-    const weekNumber = Math.ceil((daysPassed + startOfMonth.getDay() + 1) / 7);
-
-    return weekNumber;
-  }
-
-  const getDayOfWeek = day => {
-    if (day < 0 || day > 6) throw new Error('Не существует такого дня недели');
-    if (!Number.isInteger(day)) return day + '';
-    return daysOfWeek[day];
-  };
-
-  /**
-   * Получение даты из формата “01.01.2022” в Суббота, 1 неделя Января 2022 года
-   * @param {string} date “01.01.2022”
-   * @return {string} newDate Суббота, 1 неделя Января 2022 года
-   */
-  function getDayInfo(date) {
-    const parts = date.split('.');
-    const day = parseInt(parts[0]) - 1;
-    const month = parseInt(parts[1]) - 1; 
-    const year = parts[2];
-
-    const convertDay = `${month}.${day}.${year}`;
+import { getDayInfo } from './date.js';
 
 
-
-    const newDate = new Date()
-      newDate.setDate(day);
-      newDate.setMonth(month);
-      newDate.setYear(year);      
-
-    alert(newDate);
-
-    const dayOfWeek = getDayOfWeek(newDate.getDay());
-    const weekNumber = getWeekNumberInMonth(newDate);
-    const monthName = getMonth(newDate.getMonth());
-
-    const myDate = `${dayOfWeek}, ${weekNumber} неделя ${monthName} ${newDate.getFullYear()} года`;
-    return myDate;
-  }
+ 
 
   const dates = [
-    '01.01.2023',
+    '15.12.2021',
     '10.01.2023',
     '15.01.2023',
     '22.01.2023',
@@ -143,4 +63,3 @@ window.addEventListener('DOMContentLoaded', () => {
   for (let i = 0; i < cards.length; i++) {
     setDate(cards[i], dates[i]);
   }
-});
